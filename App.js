8a3b185d45a1c3e426f7b40bb2000f486d706686
ARYAN0021 <77781900+ARYAN0021@users.noreply.github.com>
2021-09-28 23:35:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs' 
-import WriteComplaint from './screens/WriteComplaint'
-import ReadComplaint from './screens/ReadComplaint'
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import WriteComplaint from './screens/WriteComplaint';
+import ReadComplaint from './screens/ReadComplaint';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
